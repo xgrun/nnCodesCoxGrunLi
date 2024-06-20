b13a7b32ed2ee74aa27231bcfff40ba30bbbb319
Xavier Grundler <xgrundler@leomail.tamuc.edu>
2024-06-20 17:40:47 -0500

    added MSE calculations and plot for test data added timers for training and predictions for model

--- SLinversion.py
+++ SLinversion.py
@@ -13,6 +13,7 @@ from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import make_scorer
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler as SS
+import time
 from eli5.sklearn import PermutationImportance
 
 columns = ["xsection","incompressability","v1n","v2n","v1p","v2p"] #names for data, explained in Cox, Grundler and Li
@@ -27,17 +28,21 @@ scaling2 = SS()
 xset = dataset[["v1p","v2p"]].to_numpy()
 yset = dataset[["xsection","incompressability"]].to_numpy()
 
+#scale data to be between -1 and 1
+xset = scaling.fit_transform(xset)
+yset = scaling2.fit_transform(yset)
+
 #split data: 75% training, 25% testing
 xtr, xte, ytr, yte = splitSet(xset, yset, test_size = .25)
 
-#scale the training data to be between -1 and 1, then apply that same transformation to the testing data
-xtr = scaling.fit_transform(xtr)
-xte = scaling.transform(xte)
-ytr = scaling2.fit_transform(ytr)
-yte = scaling2.transform(yte)
+start = time.perf_counter() #begin counter for training model
 
 #configure multi-layer perceptron regressor and train it
 model = MLPRegressor(hidden_layer_sizes=(2,6,6,2), activation = 'tanh', solver= 'lbfgs', verbose = True, max_iter = 150).fit(xtr,ytr)
+
+end = time.perf_counter() #edn counter for training model
+
+print("Time to train model: ",end-start,"s") #print time to train model
 
 #find the importance and standard deviation of each input feature (column) in determining the output using eli5
 perm = PermutationImportance(model, random_state = 133239, n_iter = 30, scoring = make_scorer(mse)).fit(xtr,ytr)
@@ -55,17 +60,24 @@ true = scaling2.inverse_transform(yte)
 truex = true[:,0]
 truek = true[:,1]
 
-#line of perfection for graphing purposes
-skpred = scaling2.inverse_transform(model.predict(xte).reshape(-1,2)) #make predictions
-skpredx = skpred[:,0] #divide into X and K
-skpredk = skpred[:,1]
-perfLinex = np.linspace(np.min(skpredx) - .05*np.min(skpredx), np.max(skpredx) + .05*np.max(skpredx)) #make lines
-perfLinek = np.linspace(np.min(skpredk) - .05*np.min(skpredk), np.max(skpredk) + .05*np.max(skpredk))
+start = time.perf_counter() #begin counter to time how long it takes to make a prediction
+
+pred = scaling2.inverse_transform(model.predict(xte).reshape(-1,2)) #make predictions
+
+end = time.perf_counter() #end counter
+
+predx = pred[:,0] #divide into X and K
+predk = pred[:,1]
+perfLinex = np.linspace(np.min(predx) - .05*np.min(predx), np.max(predx) + .05*np.max(predx)) #make lines of perfection
+perfLinek = np.linspace(np.min(predk) - .05*np.min(predk), np.max(predk) + .05*np.max(predk))
+
+numPred = np.size(predx)
+print("Time per prediction: ",(end-start)/numPred,"s") #output normalized time per prediction
 
 #prediction vs. perfection for X, graph
 plt.figure()
 plt.plot(perfLinex,perfLinex, color = 'red')
-plt.scatter(skpredx,truex,facecolors = 'none',edgecolors = 'black',label = F"R\u00B2: {np.round(score,3)}")
+plt.scatter(predx,truex,facecolors = 'none',edgecolors = 'black',label = F"R\u00B2: {np.round(score,3)}")
 plt.xlabel(r"Predicted X")
 plt.ylabel(r"True X")
 plt.legend(loc = 'upper left',fontsize = 8)
@@ -74,11 +86,31 @@ plt.savefig(f'graphOfX.pdf',format = 'pdf')
 #prediction vs. perfection for K, graph
 plt.figure()
 plt.plot(perfLinek,perfLinek,color = 'red')
-plt.scatter(skpredk,truek,facecolors = 'none',edgecolors = 'black',label = F"R\u00B2: {np.round(score,3)}")
+plt.scatter(predk,truek,facecolors = 'none',edgecolors = 'black',label = F"R\u00B2: {np.round(score,3)}")
 plt.xlabel(r"Predicted K (MeV)")
 plt.ylabel(r"True K (MeV)")
 plt.legend(loc = 'upper left',fontsize = 8)
 plt.savefig(f'graphOfK.pdf',format = 'pdf')
+
+#calculate inversion error for test data set
+dnnError = np.zeros(numPred)
+predCount = np.zeros(numPred)
+for i in range(numPred):
+    dnnError[i] = (pred[i,0] - true[i,0])**2 + (pred[i,1]/200 - true[i,1]/200)**2 #K values divided by 200 so it's the same order of magnitude as X
+    predCount[i] = i + 1
+
+#calculate inversion error for test data set
+mseTest = np.sum(dnnError)/numPred
+print("numPred = ",numPred)
+print("MSE_test = ",mseTest)
+
+#plot error
+plt.figure()
+plt.scatter(predCount,dnnError,facecolors = 'none',edgecolors = 'red',label = F"MSE: {np.round(mseTest,5)}")
+plt.xlabel(r"Prediction Number")
+plt.ylabel(r"DNN Error")
+plt.legend(loc = 'upper left',fontsize = 8)
+plt.savefig(f'predError.pdf',format = 'pdf')
 
 #write the configuration of the model
 with open("modelConfiguration.txt","w") as modConfig:
