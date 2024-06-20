cdf1c8d5787bf677e2359e2779d4af628b3c0c75
Xavier Grundler <xgrundler@leomail.tamuc.edu>
2024-06-20 16:12:04 -0500

    update dnnError to only take into account the test set add timers to track training and prediction times

--- SLemulator.py
+++ SLemulator.py
@@ -13,6 +13,7 @@ from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import make_scorer
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import MaxAbsScaler as MAS
+import time
 from eli5.sklearn import PermutationImportance
 
 columns = ["xsection","incompressability","v1n","v2n","v1p","v2p"] #names for data, explained in Cox, Grundler and Li
@@ -27,15 +28,21 @@ scaling2 = MAS()
 xset = dataset[["xsection","incompressability"]].to_numpy()
 yset = dataset[["v1p","v2p"]].to_numpy()
 
-#scale the training data to be between -1 and 1, then apply that same transformation to the testing data
+#scale data to be between -1 and 1
 xset = scaling.fit_transform(xset)
 yset = scaling2.fit_transform(yset)
 
 #split data: 75% training, 25% testing
 xtr, xte, ytr, yte = splitSet(xset, yset, test_size = .25)
 
+start = time.perf_counter() #begin counter for training model
+
 #configure multi-layer perceptron regressor and train it
 model = MLPRegressor(hidden_layer_sizes=(2,6,6,2), activation = 'tanh', solver= 'lbfgs', verbose = True, max_iter = 80).fit(xtr,ytr)
+
+end = time.perf_counter() #end counter for training model
+
+print("Time to train model: ",end-start,"s") #print time to train model
 
 #find the importance and standard deviation of each input feature (column) in determining the output using eli5
 perm = PermutationImportance(model, random_state = 133239, n_iter = 30, scoring = make_scorer(mse)).fit(xtr,ytr)
@@ -53,12 +60,19 @@ truev = scaling2.inverse_transform(yte)
 truev1 = truev[:,0]
 truev2 = truev[:,1]
 
-#line of perfection for graphing purposes
+start = time.perf_counter() #begin counter to time how long it takes to make a prediction
+
 pred = scaling2.inverse_transform(model.predict(xte).reshape(-1,2)) #make predictions
+
+end = time.perf_counter() #end counter
+
 predv1 = pred[:,0] #divide into v1 and v2
 predv2 = pred[:,1]
-perfLinev1 = np.linspace(np.min(predv1) - .05*np.min(predv1), np.max(predv1) + .05*np.max(predv1)) #make lines
+perfLinev1 = np.linspace(np.min(predv1) - .05*np.min(predv1), np.max(predv1) + .05*np.max(predv1)) #make lines of perfection
 perfLinev2 = np.linspace(np.min(predv2) - .05*np.min(predv2), np.max(predv2) + .05*np.max(predv2))
+
+numPred = np.size(predv1)
+print("Time per prediction: ",(end-start)/numPred,"s") #output normalized time per prediction
 
 #prediction vs. perfection for v1, graph
 plt.figure()
@@ -79,21 +93,25 @@ plt.legend(loc = 'upper left',fontsize = 8)
 plt.savefig(f'graphOfv2.pdf',format = 'pdf')
 
 #calculate emulator error for entire data set
-yset = scaling2.inverse_transform(yset)
-fullPred = scaling2.inverse_transform(model.predict(xset).reshape(-1,2))
-dnnError = np.zeros(89)
-runNum = np.zeros(89)
-for i in range(89):
-    dnnError[i] = (fullPred[i,0] - yset[i,0])**2 + (fullPred[i,1] - yset[i,1])**2
-    runNum[i] = i + 1
+dnnError = np.zeros(numPred)
+predCount = np.zeros(numPred)
+for i in range(numPred):
+    dnnError[i] = (pred[i,0] - truev[i,0])**2 + (pred[i,1] - truev[i,1])**2
+    predCount[i] = i + 1
+
+#calculate MSE for test set
+mseTest = np.sum(dnnError)/numPred
+print("numPred = ",numPred)
+print("MSE_test = ",mseTest)
 
 #plot error
 plt.figure()
-plt.scatter(runNum,dnnError,facecolors = 'none',edgecolors = 'red')
-plt.xlabel(r"Run Number")
+plt.scatter(predCount,dnnError,facecolors = 'none',edgecolors = 'red',label = F"MSE: {np.round(mseTest,5)}")
+plt.xlabel(r"Prediction Number")
 plt.ylabel(r"DNN Error")
+plt.legend(loc = 'upper left',fontsize = 8)
 plt.savefig(f'predError.pdf',format = 'pdf')
 
 #write the configuration of the model
-with open("modelConfiguration.txt","w") as modConfig:
+with open("modelConfigurationSLemu.txt","w") as modConfig:
     print(model.get_params(),file=modConfig)
